486c5fdbc5e583afb36e82a49cd9822eb330aaf9
Freja <frsj@itu.dk>
2025-10-27 15:33:44 +0100

    We have come far, but not far enough.
    Co-authored by: cfjo@itu.dk and annar@itu.dk

--- client/client.go
+++ client/client.go
@@ -7,10 +7,20 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"sync"
+	"time"
 
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/credentials/insecure"
 )
+
+type chatClient struct {
+	client   proto.ChatDatabaseClient
+	stream   proto.ChatDatabase_ChitChattingClient
+	username string
+	ctx      context.Context
+	wg       sync.WaitGroup
+}
 
 func main() {
 	conn, err := grpc.NewClient("localhost:5050", grpc.WithTransportCredentials(insecure.NewCredentials()))
@@ -18,24 +28,33 @@ func main() {
 		log.Fatalf("Not working")
 	}
 
-	//clienten printer?
-
 	reader := bufio.NewReader(os.Stdin)
 	username := connectClient(reader)
 
-	client := proto.NewITUDatabaseClient(conn)
+	client := proto.NewChatDatabaseClient(conn)
 
-	students, err := client.GetStudents(context.Background(), &proto.Empty{})
+	stream, err := client.ChitChatting(context.Background())
 	if err != nil {
 		log.Fatalf("Not working")
 	}
 
-	for {
-		fmt.Println("Enter a message")
-		message, _ := reader.ReadString('\n')
-		log.Println(message)
-
+	joinMessage := &proto.ChitChat{
+		Username: username,
 	}
+
+	err = stream.Send(joinMessage)
+	if err != nil {
+		log.Fatalf("u aint joinin noffin")
+	}
+
+	chatClient := &chatClient{
+		client:   client,
+		stream:   stream,
+		username: username,
+		ctx:      context.Background(),
+	}
+
+	chatClient.initiateSmallTalk(reader)
 
 }
 
@@ -45,9 +64,64 @@ func connectClient(reader *bufio.Reader) string {
 
 	if username == "" {
 		fmt.Println("Please enter a username of at least 1 character")
+		return username
 	} else {
 		fmt.Println("Your displayname:", username)
 		return username
+	}
+
+}
+
+func (c *chatClient) initiateSmallTalk(reader *bufio.Reader) {
+	fmt.Printf("Chat, or else...")
+	fmt.Println("Type '/quit' to leave the chitchatting... if you dare.")
+	fmt.Println("︵‿︵‿୨♡୧‿︵‿︵")
+
+	c.wg.Add(1)
+	go c.receiveMessage()
+
+	for { //loop for messages the client sends
+		message, _ := reader.ReadString('\n')
+
+		if message == "/quit" {
+			fmt.Println("you quit. but i will find you.")
+			break
+		}
+
+		if len(message) > 128 {
+			fmt.Println("Message is too long. Maximum is 128 characters. Yours %d", len(message))
+			continue
+		}
+
+		chitChatMsg := &proto.ChitChat{
+			Username:  c.username,
+			Message:   message,
+			Timestamp: time.Now().Format(time.RFC3339),
+		}
+
+		err := c.stream.Send(chitChatMsg)
+		if err != nil {
+			log.Fatalf("Failed to send message: %v", err)
+			break
+		}
+
+	}
+
+}
+
+func (c *chatClient) receiveMessage() {
+	defer c.wg.Done()
+
+	for {
+		msg, err := c.stream.Recv()
+		if err != nil {
+			log.Fatalf("something went wrong:", err)
+		}
+		if msg.Username == "Server" {
+			fmt.Println(msg.Message)
+		} else {
+			fmt.Printf("%s: %v", msg.Username, msg.Message)
+		}
 	}
 }
 
--- server/server.go
+++ server/server.go
@@ -2,40 +2,119 @@ package main
 
 import (
 	proto "ITUServer/grpc"
-	"context"
 	"log"
 	"net"
+	"sync"
+	"time"
 
 	"google.golang.org/grpc"
 )
 
-type ITU_databaseServer struct {
-	proto.UnimplementedITUDatabaseServer
-	students []string
+type ChatDatabaseServerITU_WOMEN_IN_STEM struct {
+	proto.UnimplementedChatDatabaseServer
+	mu      sync.RWMutex   //mutex prevents race conditions
+	clients []activeClient //(pizza)slice that keeps track of the clients on the server
 }
 
-func (s *ITU_databaseServer) GetStudents(ctx context.Context, in *proto.Empty) (*proto.Students, error) {
-	return &proto.Students{Students: s.students}, nil
+// active client that is connected to the stream
+type activeClient struct {
+	username string
+	stream   proto.ChatDatabase_ChitChattingServer
+}
+
+func (s *ChatDatabaseServerITU_WOMEN_IN_STEM) ChitChatting(stream proto.ChatDatabase_ChitChattingServer) error {
+	firstMessage, err := stream.Recv()
+	if err != nil {
+		log.Fatalf("did not recieve message: %s", err)
+	}
+
+	username := firstMessage.Username
+	if username == "" {
+		username = "Jesus Christensen" //default name #AMEN
+	}
+
+	//registering a client:
+	s.mu.Lock()             //prevents concurrent access.
+	client := activeClient{ //creating a variable of type activeClient
+		username: username, //username from first message
+		stream:   stream,   //bidirectional stream
+	}
+	s.clients = append(s.clients, client) //adds client to slice
+	s.mu.Unlock()                         //unlocks
+
+	log.Printf("user %s joined the ChitChat!<3", username)
+
+	go s.BroadcastToAll(&proto.ChitChat{
+		Message:   "User:" + username + "joined the chat!<3",
+		Username:  "Server",
+		Timestamp: time.Now().Format(time.RFC3339),
+	})
+
+	for {
+		msg, err := stream.Recv()
+		if err != nil { // when terminal is closed the user leaves the server.
+			log.Printf("%v left the chat! ;(", username) //måske behøver den ikk skrive noget 
+			s.RemoveClient(username)
+			break
+		}
+
+		go s.BroadcastToAll(msg)
+		continue
+	}
+	return nil
 }
 
 func main() {
-	server := &ITU_databaseServer{students: []string{}}
+
+	server := &ChatDatabaseServerITU_WOMEN_IN_STEM{}
 
 	server.start_server()
 }
 
-func (s *ITU_databaseServer) start_server() {
+func (s *ChatDatabaseServerITU_WOMEN_IN_STEM) start_server() {
 	grpcServer := grpc.NewServer()
 	listener, err := net.Listen("tcp", ":5050")
 	if err != nil {
 		log.Fatalf("Did not work")
 	}
 
-	proto.RegisterITUDatabaseServer(grpcServer, s)
+	proto.RegisterChatDatabaseServer(grpcServer, s)
 
 	err = grpcServer.Serve(listener)
 
 	if err != nil {
 		log.Fatalf("Did not work")
 	}
+	log.Println("Server opened")
+}
+
+func (s *ChatDatabaseServerITU_WOMEN_IN_STEM) BroadcastToAll(msg *proto.ChitChat) {
+
+	for _, client := range s.clients { // for each loop
+		go s.SendToOne(msg, client.stream, client.username) //starts a goroutine for each client
+	}
+}
+
+func (s *ChatDatabaseServerITU_WOMEN_IN_STEM) SendToOne(msg *proto.ChitChat, stream proto.ChatDatabase_ChitChattingServer, username string) {
+	err := stream.Send(msg) //sends message to client
+	if err != nil {         //in case it fails:
+		log.Printf("Failed to send message to %s: %v", username, err)
+	}
+
+}
+
+func (s *ChatDatabaseServerITU_WOMEN_IN_STEM) RemoveClient(username string) {
+	s.mu.Lock()
+	defer s.mu.Unlock()
+
+	for i, client := range s.clients {
+		if client.username == username {
+			s.clients = append(s.clients[:i], s.clients[i+1:]...)
+			log.Printf("User %s removed from client list", username)
+
+			break
+		}
+
+	}
+
 }
